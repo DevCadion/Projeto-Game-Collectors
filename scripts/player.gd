0b985dddc3ab8a71673c85920c356d5cbfa67e10
DevCadion <caio.santos.m@hotmail.com>
2025-03-26 11:10:11 -0300

    MovePlayer

---
+++ scripts/player.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D  
+
+var speed = 300  
+
+func _physics_process(delta):
+	var direction = 0  
+
+	if Input.is_action_pressed("ui_left") or Input.is_action_pressed("esquerda"):
+		direction -= 1
+	if Input.is_action_pressed("ui_right") or Input.is_action_pressed("direita"):
+		direction += 1
+
+	velocity.x = direction * speed  
+
+	move_and_slide()  
